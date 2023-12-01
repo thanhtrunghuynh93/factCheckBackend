88a541ee40a097098ae9324d97f863a1157abf9b
MinhTam <pminhtamnb@gmail.com>
2023-12-01 10:13:28 +0700

    get abstract of paper

--- app/server/models/factCheck/GPT_agent_factCheck/openalex_api.py
+++ app/server/models/factCheck/GPT_agent_factCheck/openalex_api.py
@@ -13,6 +13,16 @@ def fetch_wikidata(wikidata_id):
 def clean_str(p):
   return p.encode().decode("unicode-escape").encode("latin1").decode("utf-8")
 
+
+def create_abstract(invtext):
+    wordlist = {}
+    for k in invtext:
+        for p in invtext[k]:
+            wordlist[p] = k
+    textlength = len(wordlist.keys())
+
+    words = [wordlist[i] if i in wordlist else ' ' for i in range(textlength)]
+    return ' '.join(words)
 def get_page_obs(page):
     # find all paragraphs
     paragraphs = page.split("\n")
@@ -25,6 +35,11 @@ def get_page_obs(page):
     sentences = [s.strip() + '.' for s in sentences if s.strip()]
     return ' '.join(sentences[:5])
 def wiki_crawler(url):
+    """
+    :param url: url to a wikipedia page
+    filters all text in the wiki page
+    :return: text in the page
+    """
     response_text = requests.get(url).text
     soup = BeautifulSoup(response_text, features="html.parser")
     result_divs = soup.find_all("div", {"class": "mw-search-result-heading"})
@@ -42,9 +57,14 @@ def wiki_crawler(url):
         obs = get_page_obs(page_z)
     return obs
 
-def get_concepts(drug):
-    print("Crawl the evidence links from Google Search")
-    url = f'https://api.openalex.org/concepts?search={drug}'
+def get_concepts(concept):
+    """
+    :param concept: a concept
+    get information about the concept: wiki pages, define of concept and relation nodes of the concept
+    :return: information about the concept
+    """
+    print("Crawl the evidence links from Open Alex")
+    url = f'https://api.openalex.org/concepts?search={concept}'
     wiki_re = ''
     wikidata_json = ''
     r = requests.get(url.strip()).json()
@@ -63,9 +83,41 @@ def get_concepts(drug):
     # pdb.set_trace()
     return wiki_re, wikidata_json
 
+def get_works_concepts(concept : str):
+    """
+    :param concept: a concept
+    :return: dictionary with id of paper and abstract of this paper
+    [
+        {
+            'id': ''
+            'abstract': ''
+        }
+    ]
+    """
+    print("Crawl the evidence links from Open Alex")
+    url = f'https://api.openalex.org/works?search={concept}'
+    r = requests.get(url.strip()).json()
+    results = r['results']
+    abstract_list = []
+    for re in results:
+        id = re['id']
+        title = re['title']
+        open_access = re['open_access']
+        abstract_inverted_index = re['abstract_inverted_index']
+        # print(abstract_inverted_index)
+        if abstract_inverted_index is not None:
+            abstract = create_abstract(abstract_inverted_index)
+            abstract_dict = {}
+            abstract_dict['id'] = id
+            abstract_dict['title'] = title
+            abstract_dict['abstract'] = abstract
+            abstract_list.append(abstract_dict)
+    return abstract_list
 
 if __name__ == "__main__":
-    wiki_re, wikidata_json = get_concepts("Creatine")
-    print(wiki_re)
-    print(wikidata_json)
+    # wiki_re, wikidata_json = get_concepts("Creatine")
+    # print(wiki_re)
+    # print(wikidata_json)
+    abstract_dict = get_works_concepts("Creatine")
+    print(abstract_dict)
     pass
